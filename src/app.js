4c678775dddbc6adbec299a9610f717fea7151b1
alekhranjan75 <alekhranjan75@gmail.com>
2020-04-09 14:07:00 +0530

    Setting up react

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './app.css';
+class App extends Component {
+    render() {
+        return ( 
+        <div className = "App" >
+            <h1 > Hi I 'm React</h1> 
+        </div>
+        );
+    }
+}
+export default App;

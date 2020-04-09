5a34dfbbd0043efc99924bd7e5674f86bc442e18
alekhranjan75 <alekhranjan75@gmail.com>
2020-04-09 15:06:30 +0530

    Creating functional Components

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import './app.css';
+import Person from './Person/Person.js';
 class App extends Component {
     render() {
         return ( 
         <div className = "App" >
             <h1 > Hi I 'm React</h1> 
+            <Person />
         </div>
         );
     }
